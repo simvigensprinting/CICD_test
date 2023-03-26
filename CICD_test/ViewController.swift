a757c2cea03e2e1ef58ed1e9d5e70edc8cb5750c
simvigen <sim_vigen@yahoo.com>
2023-03-26 20:49:44 +0400

    Initial Commit

---
+++ CICD_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICD_test
+//
+//  Created by Vigen Simonyan on 26.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
